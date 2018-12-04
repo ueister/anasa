4ba566a30dbc78dff0d5d6b28b8301dc7764026e
Uei Lam <ulam@sanrio.com>
2018-12-04 11:19:05 -0800

    css, add footer, add header

---
+++ src/Footer.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export class Footer extends React.Component {
+  render() {
+    return (
+		<footer class="global-footer">
+			<p>Built by a wonderful developer for Asana.</p>
+		</footer>
+    );
+  }
+}
