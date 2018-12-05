feb4f9b85c12148588dc46d23b7b6332ee367d15
Uei Lam <ulam@sanrio.com>
2018-12-04 17:02:02 -0800

    add gallery

--- src/index.js
+++ src/index.js
@@ -3,16 +3,35 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import { Header } from './Header';
 import { Footer } from './Footer';
+import dogs from './dogs.json';
+
+const data = dogs;
+
+class Dogs extends React.Component {
+  render() {
+    return (
+		<p>{this.props.gallery}</p>
+	);
+  }
+}
 
 class Asana extends React.Component {
   render() {
+  	let gallery = data.dogs.map(function(images){
+      return (<div>{images.image}</div>);
+    });
+
     return (
     	<div>
 			<Header />
+			<Dogs gallery={gallery} />
+/*
+			<div>{gallery}</div>
+*/
 			<Footer />
 		</div>
 	);
   }
-};
+}
 
 ReactDOM.render(<Asana />, document.getElementById('root'));
