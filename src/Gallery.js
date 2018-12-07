e63161c3fa454bb83fe1365d235a8c548fb1104a
Uei Lam <ulam@sanrio.com>
2018-12-06 17:54:31 -0800

    gallery changes

--- src/Gallery.js
+++ src/Gallery.js
@@ -1,54 +1,20 @@
 import React, { Component } from 'react';
-import { Grid, Image, Row, Col, Modal, Button } from 'react-bootstrap';
+import { Grid, Row } from 'react-bootstrap';
+import dogs from './dogs.json';
+import Thumbnails from './Thumbnails';
+
+const data = dogs;
 
 export class Gallery extends Component {
-  constructor(props, context) {
-    super(props, context);
-
-    this.handleShow = this.handleShow.bind(this);
-    this.handleClose = this.handleClose.bind(this);
-
-    this.state = {
-      show: false
-    };
-  }
-
-  handleClose() {
-    this.setState({ show: false });
-  }
-
-  handleShow() {
-    this.setState({ show: true });
-  }
-
   render() {
       return (
-<Grid>
-  <Row>
-    <Col xs={6} md={4} style={{paddingBottom:10}}>
-      <Image src="https://images.unsplash.com/photo-1528113668843-f1bfaf2df681?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&dpr=1&auto=format&fit=crop&w=1000&q=80&cs=tinysrgb" responsive onClick={this.handleShow} />
-        <Modal show={this.state.show} onHide={this.handleClose}>
-          <Modal.Header closeButton>
-            <Modal.Title>Modal heading</Modal.Title>
-          </Modal.Header>
-          <Modal.Body>
-            <Image src="https://images.unsplash.com/photo-1528113668843-f1bfaf2df681?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&dpr=1&auto=format&fit=crop&w=1000&q=80&cs=tinysrgb" responsive />
-          </Modal.Body>
-          <Modal.Footer>
-            <Button onClick={this.handleClose}>Close</Button>
-          </Modal.Footer>
-        </Modal>
-    </Col>
-    <Col xs={6} md={4} style={{paddingBottom:10}}>
-      <Image src={require('./assets/images/raw/4.jpeg')} responsive />
-    </Col>
-    <Col xs={6} md={4} style={{paddingBottom:10}}>
-      <Image src="https://images.unsplash.com/photo-1528113668843-f1bfaf2df681?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&dpr=1&auto=format&fit=crop&w=1000&q=80&cs=tinysrgb" responsive />
-    </Col>
-  </Row>
-</Grid>
+		<Grid>
+			<Row class="row-eq-height">
+				{data.dogs.map(function(images, index){
+					return (<Thumbnails gallery={images.image} />);
+				})}
+			</Row>
+		</Grid>
     );
-    
   }
-  
 }
--- src/index.js
+++ src/index.js
@@ -1,30 +1,14 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/css/bootstrap-theme.min.css';
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import { Header } from './Header';
 import { Footer } from './Footer';
 import { Gallery } from './Gallery';
-import dogs from './dogs.json';
 
-const data = dogs;
-
-class Dogs extends React.Component {
+class Asana extends Component {
   render() {
-    return (
-		<p>{this.props.gallery}</p>
-	);
-  }
-}
-
-class Asana extends React.Component {
-  render() {
-  	let gallery = data.dogs.map(function(images){
-  		var path = require('.'+images.image);
-      return (<img src={path} alt="{images.source}" />);
-    });
-
     return (
     	<div>
 			<Header />
